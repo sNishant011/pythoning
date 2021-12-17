30cadd1c2a68bfad730449944f4cdac08c27a3fa
sNishant011 <shresthanishant011@gmail.com>
2021-12-17 18:08:48 +0545

    fixed issues

--- LeetCodes/medium/add_two_numbers.py
+++ LeetCodes/medium/add_two_numbers.py
@@ -1,3 +1,4 @@
-class Solution:
-	def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-		...
+def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+	print(l1, l2)
+
+addTwoNumbers([1,2,3], [3, 4, 5])
